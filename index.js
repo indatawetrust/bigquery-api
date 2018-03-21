98ad80675170e7967c06d70ac89f2b4e830e8d44
Ahmet Simsek <iamaroott@gmail.com>
2018-03-21 14:53:40 +0200

    first

---
+++ index.js
@@ -0,0 +1,107 @@
+const BigQuery = require('@google-cloud/bigquery');
+const to = require('await-to-js').default;
+
+const getTables = async ({ bigquery, datasetId, tables, _tables }) => {
+
+  let err;
+
+  const dataset = bigquery.dataset(datasetId);
+
+  [err, tables] = await to(Promise.all(tables.map(async table => {
+    if (_tables.filter(({id}) => id == table.name).length) {
+      return await Promise.resolve({
+        [table.name]: dataset
+                      .table(table.name)
+      })
+    } else {
+      [err] = await to(dataset.createTable(table.name, table.options));
+
+      if (err) {
+        return Promise.reject(new Error(`table could not be created.`));
+      }
+
+      return await Promise.resolve({
+        [table.name]: dataset
+                      .table(table.name)
+      })
+    }
+
+  })));
+
+  return await Promise.resolve({
+    bigquery,
+    table: tables.reduce((prev, next) => Object.assign(prev, next))
+  });
+
+}
+
+const main = async (opts) => {
+
+  opts = opts || {};
+
+  for (let key of ['projectId', 'keyFilename', 'datasetId']) {
+    if (!opts[key]) {
+      return await Promise.reject(new Error(`${key} required`));
+    }
+  }
+
+  let { datasetId, tables, projectId, keyFilename } = opts;
+
+  const bigquery = new BigQuery({
+    projectId,
+    keyFilename
+  });
+
+  let err, datasets, _tables;
+
+  [err, datasets] = await to(bigquery.getDatasets());
+
+  if (err) {
+    return await Promise.reject(new Error(`datasets could not be retrieved.`));
+  }
+
+  datasets = datasets[0];
+
+  if (datasets.filter(({id}) => id == datasetId).length) {
+
+    [err, _tables] = await to(bigquery.dataset(datasetId).getTables());
+
+    _tables = _tables[0];
+
+    if (!tables && !_tables.length) {
+
+      return await Promise.resolve({});
+
+    } else {
+
+      [err, tables] = await to(getTables({ bigquery: bigquery, datasetId: datasetId, tables, _tables }));
+
+      if (err) {
+        return await Promise.reject(new Error(`tables could not be retrieved.`));
+      }
+
+      return await Promise.resolve(tables);
+
+    }
+
+  } else {
+
+    [err] = await to(bigquery.createDataset(datasetId));
+
+    if (err) {
+      return await Promise.reject(new Error(`dataset could not be created.`));
+    }
+
+    [err, tables] = await to(getTables({ bigquery: bigquery, datasetId: datasetId, tables, _tables }));
+
+    if (err) {
+      return await Promise.reject(new Error(`the tables could not be retrieved.`));
+    }
+
+    return await Promise.resolve(tables);
+
+  }
+
+}
+
+module.exports = main;
